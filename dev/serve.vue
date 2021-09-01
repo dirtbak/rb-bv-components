35f170fc49c30c2034c960f95f2a64cfd3709f32
dzhusi <dzhusi@gmail.com>
2021-09-01 18:27:18 +0600

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { RbBvComponentsSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  RbBvComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <rb-bv-components-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as RbBvComponentsSample } from './rb-bv-components-sample.vue';
