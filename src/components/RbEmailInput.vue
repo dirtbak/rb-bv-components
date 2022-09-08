026aac834db295cf424d1875729e8d09f9c3aef9
mrDiablo <roman@mr-diablo.ru>
2022-09-08 20:31:19 +0600

    add emit.input -> RbEmailInput && v2.3.9

--- src/components/RbEmailInput.vue
+++ src/components/RbEmailInput.vue
@@ -22,6 +22,7 @@
                  :placeholder="placeholder"
                  autocomplete="off"
                  v-on:keyup.enter="onKeyEnter"
+                 v-on:keyup.space="onKeyEnter"
                  v-on:keyup.esc="onKeyEsc"
                  @input="onInput"
         />
@@ -76,6 +77,7 @@ export default {
         },
         selectedItems() {
             this.selectedItems = this.selectedItems === null ? [] : this.selectedItems
+            this.$emit('input', this.selectedItems)
         }
     },
     computed: {
