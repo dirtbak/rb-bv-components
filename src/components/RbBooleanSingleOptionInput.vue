d028b18d756298ef151402b5d1e4641a6d22064f
sanyaso <aytan.s@bk.ru>
2024-11-27 13:37:09 +0500

    --

--- src/components/RbBooleanSingleOptionInput.vue
+++ src/components/RbBooleanSingleOptionInput.vue
@@ -1,16 +1,16 @@
 <template>
   <b-button-group class="rb-boolean-single-option-input rb-single-option-input" :class="cls">
     <b-button class="rb-false-button" :disabled="disabled"
-              :variant="(nullable && value == null) ? 'light' : (!value ? 'primary': 'light')"
-              @click="setValue(false)">
+              :variant="(nullable && value == null) ? 'light' : (!model ? 'primary': 'light')"
+              @click="model = false">
       <slot name="false">
         <rb-icon :icon="falseIcon" v-if="showIcons"></rb-icon>
         <rb-text>{{ falseLabel }}</rb-text>
       </slot>
     </b-button>
     <b-button class="rb-true-button" :disabled="disabled"
-              :variant="(nullable && value == null) ? 'light' : (value ? 'primary': 'light')"
-              @click="setValue(true)">
+              :variant="(nullable && value == null) ? 'light' : (model ? 'primary': 'light')"
+              @click="model = true">
       <slot name="true">
         <rb-icon :icon="trueIcon" v-if="showIcons"></rb-icon>
         <rb-text>{{ trueLabel }}</rb-text>
@@ -45,13 +45,39 @@ export default {
         'is-valid': this.state === true,
         'w-100': this.block === true,
       };
+    },
+    model: {
+      get() {
+        return Boolean(this.value);
+      },
+      set(value) {
+          
+          if (this.nullable) {
+            if (this.value === false && value === false) {
+                this.$emit('input', null);
+                this.$emit('change', null);
+            }
+            if (this.value === true && value === true) {
+                this.$emit('input', null);
+                this.$emit('change', null);
+            }
+          } else {
+              this.$emit('input', value);
+          }
+      }
+    }
+  },
+  watch: {
+    value() {
+      console.log('value');
+      this.$emit('change', this.value);
     }
   },
   created() {
     //TODO: Костыль
-    if(this.value === null || this.value === undefined) {
-      this.setValue(false)
-    }
+    // if(this.value === null || this.value === undefined) {
+    //   this.setValue(false)
+    // }
   },
   methods: {
     setValue(newValue) {
